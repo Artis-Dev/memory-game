95f47a4939d9c64f9f8cb97362452dff5ac55f81
ArtisDev <artisdev@proton.me>
2023-12-15 15:56:23 +0200

    Remove unnecessary ESlint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,9 +19,5 @@ module.exports = {
   plugins: ['react', 'react-hooks'],
   rules: {
     'react/react-in-jsx-scope': 0,
-    // 'react/jsx-uses-react': 0,
-    // 'react-hooks/rules-of-hooks': 'error',
-    // 'react-hooks/exhaustive-deps': 'warn',
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
 };
